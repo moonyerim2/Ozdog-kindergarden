75bbff595a780dfd70b3e605d4e87380f8603ff8
moonyerim2 <moonyerim2@gmail.com>
2022-12-15 20:11:54 +0900

    feat: popup 1일 동안 보지 않기 설정 기능 추가

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,6 +1,23 @@
 const popup = document.querySelector("#popup");
-const popupClose = document.querySelector(".popup_close");
 
-popupClose.addEventListener("click", (e) => {
+const handlePopupNotOnday = {
+  setExpireDate: function (name, exp) {
+    let date = new Date();
+    date = date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
+    localStorage.setItem(name, date);
+  },
+
+  getExpireDate: function (name) {
+    let now = new Date();
+    now = now.setTime(now.getTime());
+    return parseInt(localStorage.getItem(name)) > now;
+  },
+};
+
+popup.addEventListener("click", ({ target }) => {
+  if (target.classList.contains("popup_not_oneday")) {
+    handlePopupNotOnday.setExpireDate("popup_expiration_date", 1);
+  }
+
   popup.style.display = "none";
 });
